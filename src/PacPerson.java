d4a4798491caa327cf7198423beb0db283fd256d
Angus Young <angusryoung@gmail.com>
2019-12-01 11:15:44 -0500

    trivial changes

--- src/PacPerson.java
+++ src/PacPerson.java
@@ -1,2 +1,49 @@
 public class PacPerson {
+    //Current direction "PacPerson is facing
+    String currentDirection ;
+
+    //possible directions
+    boolean up,down,left,right;
+
+    //role is either hunter or prey true -> hunter, false -> prey
+    boolean role;
+
+    //current location (might need to change how i keep location later on)
+    int x;
+    int y;
+
+
+    public PacPerson(int spawnPoint){
+        spawn(spawnPoint);
+
+    }
+    //checks
+    public void canMove(){
+
+    }
+
+    //for now we will not randomize spawn location
+    //3 spawn points
+    public void spawn(int spawnPoint){
+        if(spawnPoint == 1){
+            x = 1;
+            y = 1;
+        }
+        else if(spawnPoint == 2){
+            x = 4;
+            y = 4;
+
+        }
+        else{
+            x = 6;
+            y= 8;
+        }
+    }
+
+
+
+
+
+
+
 }
